fb068442ff28600fb43f0fda3446ded5f3a4a9b8
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-20 17:05:21 +0200

    super bonus implementato

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import {cowsay} from 'cowsayjs';
+
 console.log('hello world');
 console.log('hello boolean');
 
 
 const citazioniBelle = process.argv.slice(2)
-console.log(`Eh ${citazioniBelle}!`);
+console.log(cowsay(`Oggi è proprio una bella Giornata ${citazioniBelle}`));
+
+
